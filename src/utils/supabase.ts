a636cf0f1f057fc3f89d5ac1979075eb2f0e95c8
Ihr Name <ihre.email@example.com>
2025-08-20 17:28:46 +0200

    Version 1.8

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://qxhbxmglobiccfqspdip.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4aGJ4bWdsb2JpY2NmcXNwZGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5NzI4NzQsImV4cCI6MjA0NzU0ODg3NH0.Ej8Ej8Ej8Ej8Ej8Ej8Ej8Ej8Ej8Ej8Ej8Ej8Ej8Ej8';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4aGJ4bWdsb2JpY2NmcXNwZGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMzg3NjIsImV4cCI6MjA3MDkxNDc2Mn0.PFZQj2G1j17J9owBUiwdwzZL5bnZFFXRyAvzkrtQ9eQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
